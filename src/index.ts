e4ce88c88201cdb8e52b1bc99e4198a95e23db92
okxiaoliang4 <okxiaoliang4@gmail.com>
2022-06-22 18:36:42 +0800

    feat: options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
 import { $fetch } from 'ohmyfetch'
 
-export async function sliceDownLoad(url: string) {
-  console.log(await getContentLength(url));
+export const clamp = (n: number, min: number, max: number) => Math.min(max, Math.max(min, n))
+
+interface SliceDownLoadOptions {
+  /**
+   * 线程数
+   * @default 3
+   */
+  threads?: number
+
+  /**
+   * 块大小
+   * @default 102400
+   */
+  chunkSize?: number
+}
+
+export async function sliceDownLoad(url: string, options: SliceDownLoadOptions = {}) {
+  const {
+    threads = 6,
+    chunkSize = 102400
+  } = options
+  const contentLength = await getContentLength(url)
+  const reqs = new Array(threads)
+  const chunks = Math.ceil(contentLength / chunkSize);
+
+  // TODO: 每下载好一块就从队列中删除，并添加新的下载任务
+  // TODO: 逻辑优化
+  for (let i = 0; i < threads; i++) {
+    let start = i * chunkSize;
+    let end = i + 1 == chunks ? contentLength - 1 : (i + 1) * chunkSize - 1;
+    console.log(`bytes=${start}-${end}`);
+  }
+  
+  // Promise.race(reqs)
+}
+
+export function download(url: string, start: number, end: number) {
+  return $fetch(url, {
+    headers: {
+      Range: `bytes=${start}-${end}`
+    }
+  })
 }
 
 export function getContentLength(url: string) {
@@ -9,3 +49,4 @@ export function getContentLength(url: string) {
     method: 'HEAD'
   }).then(res => Number(res.headers.get('Content-Length')))
 }
+
