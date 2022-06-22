0cab4ccce2e90236c124a381437c8e0d21b4f6cc
okxiaoliang4 <353742991@qq.com>
2022-06-23 00:17:07 +0800

    feat: slice download

--- src/index.ts
+++ src/index.ts
@@ -18,30 +18,47 @@ interface SliceDownLoadOptions {
 
 export async function sliceDownLoad(url: string, options: SliceDownLoadOptions = {}) {
   const {
-    threads = 6,
+    threads = 3,
     chunkSize = 102400
   } = options
   const contentLength = await getContentLength(url)
-  const reqs = new Array(threads)
   const chunks = Math.ceil(contentLength / chunkSize);
+  const reqs = new Array<ReturnType<typeof download>>(chunks)
 
   // TODO: 每下载好一块就从队列中删除，并添加新的下载任务
   // TODO: 逻辑优化
-  for (let i = 0; i < threads; i++) {
+  // TODO: 进度条
+  for (let i = 0; i < chunks; i++) {
     let start = i * chunkSize;
     let end = i + 1 == chunks ? contentLength - 1 : (i + 1) * chunkSize - 1;
-    console.log(`bytes=${start}-${end}`);
+    reqs[i] = (download(i, url, start, end));
   }
-  
-  // Promise.race(reqs)
+
+  const results = await Promise.all(reqs)
+  console.log(results)
+  const blob = new Blob(results.map(item => item.result as Blob), { type: 'image/jpg' })
+  console.log(blob);
+  const r = window.URL.createObjectURL(blob)
+  console.log(r);
+  const img = document.createElement('img')
+  img.src = r
+  document.body.appendChild(img)
 }
 
-export function download(url: string, start: number, end: number) {
-  return $fetch(url, {
+export function mergeBlob(blobs: Blob[]): Blob { 
+  return new Blob(blobs, { type: 'image/jpg' })
+}
+
+export async function download(index: number, url: string, start: number, end: number) {
+  const result = await $fetch(url, {
     headers: {
       Range: `bytes=${start}-${end}`
     }
   })
+  return {
+    index,
+    result
+  }
 }
 
 export function getContentLength(url: string) {
