ff49b1714b9cc75202e052a423c461c34f6cb367
okxiaoliang4 <353742991@qq.com>
2022-06-23 00:07:47 +0800

    feat: asyncPool

--- src/index.ts
+++ src/index.ts
@@ -5,48 +5,71 @@ export const clamp = (n: number, min: number, max: number) => Math.min(max, Math
 interface SliceDownLoadOptions {
   /**
    * 线程数
-   * @default 3
+   * @default 8
    */
   threads?: number
 
   /**
    * 块大小
-   * @default 102400
+   * @default 10_240_000
    */
   chunkSize?: number
 }
 
 export async function sliceDownLoad(url: string, options: SliceDownLoadOptions = {}) {
   const {
-    threads = 3,
-    chunkSize = 102400
+    threads = 8,
+    chunkSize = 10_240_000
   } = options
   const contentLength = await getContentLength(url)
   const chunks = Math.ceil(contentLength / chunkSize);
-  const reqs = new Array<ReturnType<typeof download>>(chunks)
+  const reqs = new Array<DownloadOptions>(chunks)
+  const blobs = new Array(chunks)
 
-  // TODO: 每下载好一块就从队列中删除，并添加新的下载任务
-  // TODO: 逻辑优化
   // TODO: 进度条
   for (let i = 0; i < chunks; i++) {
     let start = i * chunkSize;
     let end = i + 1 == chunks ? contentLength - 1 : (i + 1) * chunkSize - 1;
-    reqs[i] = (download(i, url, start, end));
+    reqs[i] = { index: i, url, start, end };
   }
 
-  const results = await Promise.all(reqs)
-  console.log(results)
-  const blob = new Blob(results.map(item => item.result as Blob), { type: results[0]?.result.type })
-  console.log(blob);
-  const r = window.URL.createObjectURL(blob)
-  console.log(r);
-  const img = document.createElement('img')
-  img.src = r
-  document.body.appendChild(img)
-  
+  for await (const value of asyncPool(threads, reqs, download)) {
+    blobs[value.index] = value.result;
+  }
+
+  return new Blob(blobs, { type: blobs[0]?.type })
 }
 
-export async function download(index: number, url: string, start: number, end: number) {
+export async function* asyncPool<IN, OUT>(concurrency: number, iterable: ReadonlyArray<IN>, iteratorFn: (item: IN, iterable?: ReadonlyArray<IN>) => Promise<OUT>): AsyncIterableIterator<OUT> {
+  const executing = new Set<Promise<IN>>();
+  async function consume() {
+    const [promise, value] = await Promise.race(executing) as unknown as [Promise<IN>, OUT];
+    executing.delete(promise);
+    return value;
+  }
+  for (const item of iterable) {
+    const promise = (async () => await iteratorFn(item, iterable))().then(
+      value => [promise, value]
+    ) as Promise<IN>;
+    executing.add(promise);
+    if (executing.size >= concurrency) {
+      yield await consume();
+    }
+  }
+  while (executing.size) {
+    yield await consume();
+  }
+}
+
+interface DownloadOptions {
+  index: number,
+  url: string,
+  start: number,
+  end: number
+}
+
+export async function download(options: DownloadOptions) {
+  const { index, url, start, end } = options
   const result = await $fetch(url, {
     responseType: 'blob',
     headers: {
