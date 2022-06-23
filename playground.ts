9642b3b5768c062e8d6d2ec0e50a3570f0dc0c2c
okxiaoliang4 <okxiaoliang4@gmail.com>
2022-06-23 18:08:30 +0800

    fix: wrong file

--- playground.ts
+++ playground.ts
@@ -3,7 +3,7 @@ import { telecharger } from './src/index'
 const button = document.querySelector('#download')
 button?.addEventListener('click', async () => {
   console.time('download')
-  const result = await telecharger('/CCA-universalTest-v2.0.1.3.apk')
+  const result = await telecharger('/test.jpg')
   result.emitter.on('progress', (progress) => {
     console.log(`${progress * 100}%`);
   })
